80c654c99a5ab7e9bf3311255548891ee8311d97
Nikola <nikolamilijevic5@gmail.com>
2026-05-18 11:24:40 +0200

    Fix clerk redirect after sign in and sign up

--- frontend/app/sign-in/[[...sign-in]]/page.tsx
+++ frontend/app/sign-in/[[...sign-in]]/page.tsx
@@ -3,7 +3,7 @@ import { SignIn } from "@clerk/nextjs";
 export default function SignInPage() {
   return (
     <div style={{ display: "flex", justifyContent: "center", marginTop: "100px" }}>
-      <SignIn />
+      <SignIn forceRedirectUrl="/dashboard" />
     </div>
   );
 }
--- frontend/app/sign-up/[[...sign-up]]/page.tsx
+++ frontend/app/sign-up/[[...sign-up]]/page.tsx
@@ -3,7 +3,7 @@ import { SignUp } from "@clerk/nextjs";
 export default function SignUpPage() {
   return (
     <div style={{ display: "flex", justifyContent: "center", marginTop: "100px" }}>
-      <SignUp />
+      <SignUp forceRedirectUrl="/dashboard" />
     </div>
   );
 }
