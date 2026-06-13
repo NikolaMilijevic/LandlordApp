23a8dc2070fed866644f6a55d41fd2b868002ab4
Nikola <nikolamilijevic5@gmail.com>
2026-06-13 16:19:00 +0200

    Week 2: properties and tenants working, fix datetime and dto issues

---
+++ backend/LandlordApi/Migrations/20260613123250_FixedAddressTypo.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LandlordApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixedAddressTypo : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Adress",
+                table: "Properties",
+                newName: "Address");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Address",
+                table: "Properties",
+                newName: "Adress");
+        }
+    }
+}
